97e29308505fd1139a9e3922a000b5a2c7678341
luke0b1000 <luke0b1000@gmail.com>
2018-07-03 23:34:30 -0400

    1x7x2

--- index.js
+++ index.js
@@ -30,6 +30,41 @@ const TOGGLE_TODO = 'TOGGLE_TODO';
 const ADD_GOAL = 'ADD_GOAL';
 const REMOVE_GOAL = 'REMOVE_GOAL';
 
+function addTodoAction (todo) {
+    return {
+        type: ADD_TODO,
+        todo
+    }
+}
+
+function removeTodoAction (id) {
+    return {
+        type: REMOVE_TODO,
+        id
+    }
+}
+
+function toggleTodoAction (id) {
+    return {
+        type: TOGGLE_TODO,
+        id
+    }
+}
+
+function addGoalAction (goal) {
+    return {
+        type: ADD_GOAL,
+        goal
+    }
+}
+
+function removeGoalAction(id) {
+    return {
+        type: REMOVE_GOAL,
+        id
+    }
+}
+
 // APP CODE
 function todos(state = [], action) {
     switch(action.type) {
@@ -71,11 +106,10 @@ const unsubscribe = store.subscribe(()=> {
     console.log('The new state is ', store.getState());
 })
 
-store.dispatch({
-    type: ADD_TODO,
-    todo: {
+store.dispatch(addTodoAction(
+    {
         id: 0,
         name: 'Learn Redux',
         complete: false
     }
-})
+))
